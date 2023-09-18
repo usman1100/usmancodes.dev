843873d37dd43e53fc8b05c978d7bdba5aa59ca7
usman1100 <usmanahmed1100@gmail.com>
2023-09-19 00:42:18 +0500

    terminal state

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,12 +1,38 @@
-const Stdin = () => {
-  return (
-    <input className="ml-2 w-fit flex-1 border-none bg-transparent outline-none" />
-  );
-};
+"use client";
+
+import { useState } from "react";
 
 export const Terminal = () => {
+  const [value, setValue] = useState<string>("echo Hello");
+  const [commandHistory, setCommandHistory] = useState<string[]>([]);
+
+  const Stdin = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setCommandHistory([...commandHistory, value]);
+      setValue("");
+    };
+
+    return (
+      <form onSubmit={handleSubmit} className="flex items-center">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          autoFocus={true}
+          className="ml-2 w-fit flex-1 border-none bg-transparent outline-none"
+        />
+      </form>
+    );
+  };
+
   return (
     <div className="h-[50vh] w-full bg-black p-5 text-white">
+      {commandHistory.map((command) => (
+        <div className="flex items-center text-green-400" key={command}>
+          {"-"}
+          <p className="ml-2">{command}</p>
+        </div>
+      ))}
       <div className="flex items-center">
         {">"} <Stdin />
       </div>
